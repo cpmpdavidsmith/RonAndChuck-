06b17701a90ec7cc86c545ed99a5ff9118265eee
David Smith <cpmpdavidsmith@icloud.com>
2023-04-22 08:53:04 -0400

    final with correction

--- RonAndChuck/Program.cs
+++ RonAndChuck/Program.cs
@@ -6,6 +6,22 @@
         {
             var client = new HttpClient();
             var quote = new QuoteGenerator(client);
+            for (int i = 0; i<5; i++)
+            {
+                Console.WriteLine("-----------");
+                Console.WriteLine("RON SWANSON looks intently at CHUCK NORRIS and says...");
+                Thread.Sleep(1900);
+                Console.WriteLine($"{quote.RonSwanson()}");
+                Thread.Sleep(1900);
+                Console.WriteLine("");
+                Console.WriteLine("");
+                Console.WriteLine("CHUCK NORRIS speaks in third person....");
+                Thread.Sleep(1900);
+                Console.WriteLine($"{quote.ChuckNorris()}"); ;
+                Console.WriteLine("");
+                Thread.Sleep(2500);
+
+            }
         }
     }
 }
