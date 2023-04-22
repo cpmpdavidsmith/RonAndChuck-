512864c23dbaec6890a025f055265c60c9710e1e
David Smith <cpmpdavidsmith@icloud.com>
2023-04-22 07:55:54 -0400

    initial commit

---
+++ RonAndChuck/QuoteGenerator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading;
+
+namespace RonAndChuck
+{
+	public class QuoteGenerator
+	{
+		private HttpClient _client;
+
+		public QuoteGenerator(HttpClient client)
+		{
+			_client = client;
+		}
+	}
+}
+
